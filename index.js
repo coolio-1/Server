df64f272fc61d607e496867ffa1a629eddec3d2a
coolio-1 <coolio4325@gmail.com>
2016-08-05 18:59:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var express = require("express");
-var app = express();
-var port = 3700;
-
-app.get("/", function(req, res){
-    res.send("It works!");
-});
-
-app.listen(port);
-console.log("Listening on port " + port);
+var http = require("http");
+http.createServer(function(request, response) {
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.write("Hello World");
+response.end();
+}).listen(8888);
